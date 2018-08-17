7c0d04fba71c731f6e4ed1de102a79d83d316887
Matt Raible <matt.raible@okta.com>
2018-08-17 15:33:42 -0600

    Project initialize!

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CarListComponent } from './car-list/car-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
