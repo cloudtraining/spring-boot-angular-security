4124e2f06d816934bcbaddb66f64043e137ce712
github@idk.email <github@idk.email>
2020-02-09 21:05:08 -0600

    gitignore, package updates

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  httpEndpoint: 'http://localhost:8080',
+  firebaseConfig : {
+    apiKey: ' AIzaSyCh54PmNdy5kPEk-PzY_ySuAdGtESeLFwg',
+    authDomain: 'dev-nexus-2020.firebaseapp.com',
+    databaseURL: 'https://dev-nexus-2020.firebaseio.com',
+    projectId: 'dev-nexus-2020',
+    storageBucket: 'dev-nexus-2020.appspot.com',
+    messagingSenderId: '720797766768'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
