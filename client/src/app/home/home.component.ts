ed56343516b48cab08dfd886580e40188411e67e
github@idk.email <github@idk.email>
2020-02-09 21:39:58 -0600

    removing okta reference

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { OktaAuthService } from '@okta/okta-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {AuthService} from "../shared/auth/auth.service";
 import {Router} from "@angular/router";
